0e607a351b36214fc2e8a45d9f83c41c3ab2cb20
Arman091 <rasoolarman7@gmail.com>
2023-07-24 11:57:03 +0530

    update

--- src/Header.js
+++ src/Header.js
@@ -1,18 +1,41 @@
-import './Header.css'
+import styles from "./Header.module.css";
+import { IconButton } from "@mui/material";
+import DescriptionIcon from "@mui/icons-material/Description";
+import SearchIcon from "@mui/icons-material/Search";
+import AppsIcon from "@mui/icons-material/Apps";
+import SideBar from "./sidebar";
+function Header(props) {
+  const formHandler = (e) => {
+    alert(props.forms);
+  };
 
-function Header() {
-    return (
-        < div className="header">
-            <div className='header_info'>
-                <MenuIcon/>
-            </div>
-            <div className='header_search'>
-                 
-            </div>
-            <div className='header_right'>
-
-            </div>
-       </div>
-        
-    )
+  return (
+    <div className={styles.header}>
+      <SideBar />
+      <div className={styles.header_info}>
+        {/* single EventHandler */}
+        <IconButton onClick={formHandler}>
+          <DescriptionIcon color="secondary" />
+          <span className={styles.info}>Forms</span>
+        </IconButton>
+      </div>
+      <div className={styles.search}>
+        <SearchIcon
+          sx={{
+            position: "relative",
+            top: "8px",
+            size: "8em",
+          }}
+        />
+        <input type="search" placeholder="search" />
+      </div>
+      <div className={styles.right}>
+        <IconButton>
+          <AppsIcon color="secondary" />
+        </IconButton>
+      </div>
+    </div>
+  );
 }
+
+export default Header;
